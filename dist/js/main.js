e81546aee5e8026c9ad40fe741c5163d5d5ba9da
Tony Spiro <tspiro@tonyspiro.com>
2017-06-06 13:57:01 -0500

    powered by stripe

--- dist/js/main.js
+++ dist/js/main.js
@@ -602,7 +602,7 @@ angular.module("config", [])
 .constant("URL", "https://api.cosmicjs.com/v1/")
 .constant("READ_KEY", "")
 .constant("WRITE_KEY", "")
-.constant("STRIPE_KEY", "");
+.constant("STRIPE_KEY", "pk_test_oRv6WcnATRyMqponKKG6QlON");
 
 (function () {
     'use strict';
